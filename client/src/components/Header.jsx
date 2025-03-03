e901562fbd9e47d6489d1206acb1d456dccd90df
Kewen Lin <Kewenlin12@gmail.com>
2025-03-03 16:43:04 -0500

    Link header signin and register btn

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,12 +7,18 @@ const Header = () => {
     const LogoClick = () => {
         navigate('/');
     };
+    const handleSignInClick = () => {
+      navigate('./Login'); 
+    };
+    const handleRegisterClick = () => {
+      navigate('./Login'); 
+    };
   return (
       <div className="Header">
           <div onClick={LogoClick} classname="Logo-container"><img style={{ height: "75px" }} classname="Logo-img" src={logo} alt="Study Buddy"></img></div>
       <div className="Header-buttons">
-        <button className="Sign-in-button">Sign In</button>
-        <button className="Register-button">Register</button>
+        <button className="Sign-in-button" onClick = {handleSignInClick}>Sign In</button>
+        <button className="Register-button" onClick = {handleRegisterClick}>Register</button>
       </div>
     </div>
   )
